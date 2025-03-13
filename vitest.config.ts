9d4b2ca5ca2757ef8bd234a2d68caca41f5621f5
jimmyLee <jimmyLee@e.com>
2025-03-13 20:35:09 +0800

    Configure testing environment with Vitest and Testing Library

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      exclude: ['src/test/setup.ts']
+    }
+  }
+}))
